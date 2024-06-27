83356e85f04b29897c2a0feaa5c2d12968e3a97a
crispyoz <36563657+crispyoz@users.noreply.github.com>
2024-06-27 22:33:44 +1000

    Delete files directory
    Signed-off-by: crispyoz <36563657+crispyoz@users.noreply.github.com>

--- files/etc/profile.d/01_set_terminal.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-stty rows 40 cols 132
--- files/etc/tube/scripts/ports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-#
-# Check open ports on each active host
-#
-set -x
-ip_src=$(echo "$2" | cut -d. -f1-3)
-nmap -sn $2/$3 | grep -i "report for" | awk -F" " '{ print $5 }' | grep -i $(echo "$2" | cut -d. -f1-3)  | xargs nmap -v -n | grep -i "discovered open port" | awk -F" " '{ print $6 " " $4 }' | sort -n >> $4
-echo "------------------------------------" >> $4
